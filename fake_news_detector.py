93f2b2c36c556b72a82ebdb08e218a9ab26d1bb9
William <laufferw.gmail.com>
2024-08-23 08:46:35 -0600

    update: added speech txt for trianing and updated fake news detector to train on the speech.

--- fake_news_detector.py
+++ fake_news_detector.py
@@ -8,10 +8,21 @@ class FakeNewsDetector:
         self.model = self.train_model()
 
     def train_model(self):
+        # Existing data
         data = {
-            'text': ["This is real news", "This is fake news", "Another real article", "More fake content"],
-            'label': [0, 1, 0, 1]
+            'text': [
+                "This is real news", 
+                "This is fake news", 
+                "Another real article", 
+                "More fake content"
+            ],
+            'label': [0, 1, 0, 1]  # 0 for real, 1 for fake
         }
+
+        # Add the misclassified transcript to the training data
+        data['text'].append("Full transcript of the presidential speech")
+        data['label'].append(0)  # Label it as real
+
         df = pd.DataFrame(data)
 
         model = make_pipeline(TfidfVectorizer(), MultinomialNB())
